0de224720f7c68aaf36fcb083543a678daf95814
niksli-tl <ytka2008xd@gmail.com>
2025-02-20 02:07:46 +0500

    add classes for sprites,game loop and level 1,

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from pygame import time, display, transform, image, sprite, event, QUIT, key, K_UP, K_DOWN, K_LEFT, K_RIGHT
 
 FPS = 60
@@ -19,6 +21,9 @@ class GeneralSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+    def collides_with(self, enemy):
+        return self.rect.colliderect(enemy.rect)
+
 
 class PlayerSprite(GeneralSprite):
     def update(self):
@@ -38,7 +43,7 @@ class EnemyHSprite(GeneralSprite):
         super().__init__(player_image, player_x, player_y, player_step)
         self.direction = direction
         self.x1 = self.rect.x
-        self.x2 = self.rect.x + 200
+        self.x2 = self.rect.x + 300
 
     def update(self):
         if self.direction:
@@ -56,7 +61,7 @@ class EnemyVSprite(GeneralSprite):
         super().__init__(player_image, player_x, player_y, player_step)
         self.direction = direction
         self.y1 = self.rect.y
-        self.y2 = self.rect.y + 200
+        self.y2 = self.rect.y + 300
 
     def update(self):
         if self.direction:
@@ -69,22 +74,115 @@ class EnemyVSprite(GeneralSprite):
                 self.direction = True
 
 
-main_sprite = PlayerSprite('images/running_010.png', 100, 250, 10)
-enemy_sprite1 = EnemyHSprite('images/fly1.png', 500, 450, 2, True)
-enemy_sprite2 = EnemyVSprite('images/fly1.png', 1000, 300, 2, True)
+class Wall(sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = transform.scale(image.load('images/wall_block.png'), (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class WallH(Wall):
+    def __init__(self,x,y):
+        self.image = transform.scale(image.load('images/wall_h.png'), (200, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+class WallV(Wall):
+    def __init__(self,x,y):
+        self.image = transform.scale(image.load('images/wall_v.png'), (50, 200))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+def generate_treasure(x, y):
+    res = randint(1,4)
+    if res == 1:
+        return GeneralSprite('images/gem_red.png',x,y,0)
+    elif res == 2:
+        return GeneralSprite('images/gem_white.png',x,y,0)
+    elif res == 3:
+        return GeneralSprite('images/gem_blue.png',x,y,0)
+    elif res == 4:
+        return GeneralSprite('images/gem_green.png',x,y,0)
+
+
+main_sprite = PlayerSprite('images/running_010.png', 100, 250, 5)
+treasure = None
+levels = []
+walls = []
+enemies = []
+
+def load_level1():
+    global treasure
+    enemies.clear()
+    walls.clear()
+    treasure = generate_treasure(1450,800)
+    enemies.append(EnemyHSprite('images/fly1.png', 300, 600, 2, True))
+    enemies.append(EnemyVSprite('images/fly1.png', 600, 50, 2, True))
+    enemies.append(EnemyHSprite('images/fly1.png', 1000, 450, 2, True))
+    walls.append(WallV(150,0))
+    walls.append(WallH(0,300))
+    walls.append(WallH(300,200))
+    walls.append(WallV(500,200))
+    walls.append(WallV(300,250))
+    walls.append(Wall(300,450))
+    walls.append(WallH(300,500))
+    walls.append(WallH(500,500))
+    walls.append(WallV(700,350))
+    walls.append(WallV(700,200))
+    walls.append(WallH(750,200))
+    walls.append(WallV(900,200))
+    walls.append(WallH(900,350))
+    walls.append(WallV(1050,0))
+    walls.append(WallH(1200,150))
+    walls.append(WallV(1200,200))
+    walls.append(WallH(1200,350))
+    walls.append(WallV(1350,400))
+    walls.append(WallV(1350,550))
+    walls.append(WallH(1400,700))
+    walls.append(WallV(150,500))
+    walls.append(WallH(150,700))
+    walls.append(WallV(300,700))
+    walls.append(WallH(500,700))
+    walls.append(WallH(700, 700))
+    walls.append(WallV(900, 550))
+    walls.append(WallH(900, 550))
+    walls.append(WallH(1000, 550))
+    walls.append(WallV(1150, 550))
+    walls.append(WallV(1150,700))
+levels.append(load_level1)
 game = True
+current_level = 0
 # 0-game 1-lose 2-win 3-end
 state = 0
+load_level1()
 while game:
     window.blit(background, (0, 0))
-    main_sprite.reset()
-    main_sprite.update()
-    enemy_sprite1.reset()
-    enemy_sprite1.update()
-    enemy_sprite2.reset()
-    enemy_sprite2.update()
+
     if state == 0:
-        pass
+        main_sprite.reset()
+        main_sprite.update()
+        treasure.reset()
+        if main_sprite.collides_with(treasure):
+            state = 2
+        for wall in walls:
+            wall.reset()
+            if main_sprite.collides_with(wall):
+                state = 1
+        for enemy in enemies:
+            enemy.update()
+            enemy.reset()
+            if enemy.collides_with(main_sprite):
+                state = 1
+
     elif state == 1:
         pass
     elif state == 2:
